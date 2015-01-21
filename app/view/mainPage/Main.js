2b628eb820c7b248449e81d040a9abed56a3b8e8
AresCC <chenyunfei@playcrab.com>
2015-01-22 01:37:53 +0800

    no message

--- app/view/mainPage/Main.js
+++ app/view/mainPage/Main.js
@@ -2,136 +2,132 @@
  * Created by DingYuan on 2015-1-19.
  */
 Ext.define('Worker.view.mainPage.Main', {
-    extend: 'Ext.Panel',
+    extend: 'Ext.NavigationView',
     alias: 'widget.mainPage',
     config: {
-        items:[
-            {
-                xtype:'navigationview',
-                id: 'mainView',
-                navigationBar: {
-                    ui: 'light',
-                    margin : '20 0 0 0',
-                    items: [
-                        {
-                            iconMask : true,
-                            iconCls : 'locate',
-                            hidden : false,
-                            text: '报警电话',
-                            ui: 'forward',
-                            id: 'sos',
-                            align: 'left'
-                        },
-                        {
-                            iconMask : true,
-                            iconCls : 'star',
-                            hidden : false,
-
-                            text: '客服电话',
-                            id: 'serviceNum',
-                            ui: 'action-round',
-                            align: 'right'
-                        }
-                    ]
+        
+        navigationBar: {
+            ui: 'light',
+            margin : '20 0 0 0',
+            items: [
+                {
+                    iconMask : true,
+                    iconCls : 'locate',
+                    hidden : false,
+                    text: '报警电话',
+                    ui: 'forward',
+                    id: 'sos',
+                    align: 'left'
                 },
-                items: [
-                    {
-                    title: 'JOY',
+                {
+                    iconMask : true,
+                    iconCls : 'star',
+                    hidden : false,
+
+                    text: '客服电话',
+                    id: 'serviceNum',
+                    ui: 'action-round',
+                    align: 'right'
+                }
+            ]
+        },
+        items: [
+            {
+            title: 'JOY',
+            items: [
+                {
+                    xtype: 'container',
+                    layout: 'vbox',
                     items: [
                         {
                             xtype: 'container',
-                            layout: 'vbox',
+                            layout: 'hbox',
+                            border: 1,
+                            style: 'border-color: black; border-style: solid;',
                             items: [
                                 {
                                     xtype: 'container',
-                                    layout: 'hbox',
-                                    border: 1,
-                                    style: 'border-color: black; border-style: solid;',
+                                    layout: 'vbox',
+                                    width: '70%',
                                     items: [
                                         {
-                                            xtype: 'container',
-                                            layout: 'vbox',
-                                            width: '70%',
-                                            items: [
-                                                {
-                                                    xtype: 'label',
-                                                    html: '订单号:1212121212'
-                                                },
-                                                {
-                                                    xtype: 'label',
-                                                    html: '预约时间:2014-12-12'
-                                                },
-                                                {
-                                                    xtype: 'label',
-                                                    html: '预约持续时间:100'
-                                                },
-                                                {
-                                                    xtype: 'label',
-                                                    html: '用户地址:xxxxxxxxxxxxxx'
-                                                }
-                                            ]
+                                            xtype: 'label',
+                                            html: '订单号:1212121212'
                                         },
                                         {
-                                            xtype: 'button',
-                                            ui: 'decline',
-                                            width: 140,
-                                            height: 100,
-                                            text: '订单详情',
-                                            handler: function () {
-                                                panel = Ext.create('Worker.view.orderPage.OrderDetail',{title:'12121212'});
-                                                Ext.getCmp('mainView').push(panel);
-                                            }
+                                            xtype: 'label',
+                                            html: '预约时间:2014-12-12'
+                                        },
+                                        {
+                                            xtype: 'label',
+                                            html: '预约持续时间:100'
+                                        },
+                                        {
+                                            xtype: 'label',
+                                            html: '用户地址:xxxxxxxxxxxxxx'
                                         }
                                     ]
                                 },
                                 {
+                                    xtype: 'button',
+                                    ui: 'decline',
+                                    width: 140,
+                                    height: 100,
+                                    text: '订单详情',
+                                    handler: function () {
+                                        panel = Ext.create('Worker.view.orderPage.OrderDetail',{title:'12121212'});
+                                        Ext.getCmp('mainView').push(panel);
+                                    }
+                                }
+                            ]
+                        },
+                        {
+                            xtype: 'container',
+                            layout: 'hbox',
+                            border: 1,
+                            style: 'border-color: black; border-style: solid;',
+                            items: [
+                                {
                                     xtype: 'container',
-                                    layout: 'hbox',
-                                    border: 1,
-                                    style: 'border-color: black; border-style: solid;',
+                                    layout: 'vbox',
+                                    width: '70%',
                                     items: [
                                         {
-                                            xtype: 'container',
-                                            layout: 'vbox',
-                                            width: '70%',
-                                            items: [
-                                                {
-                                                    xtype: 'label',
-                                                    html: '订单号:23232323'
-                                                },
-                                                {
-                                                    xtype: 'label',
-                                                    html: '预约时间:2014-12-12'
-                                                },
-                                                {
-                                                    xtype: 'label',
-                                                    html: '预约持续时间:180'
-                                                },
-                                                {
-                                                    xtype: 'label',
-                                                    html: '用户地址:xxxxxxxxxxxxxx'
-                                                }
-                                            ]
+                                            xtype: 'label',
+                                            html: '订单号:23232323'
                                         },
                                         {
-                                            xtype: 'button',
-                                            ui: 'decline',
-                                            width: 140,
-                                            height: 100,
-                                            text: '订单详情',
-                                            handler: function () {
-                                                panel = Ext.create('Worker.view.orderPage.OrderDetail',{title:'23232323'});
-                                                Ext.getCmp('mainView').push(panel);
-                                            }
+                                            xtype: 'label',
+                                            html: '预约时间:2014-12-12'
+                                        },
+                                        {
+                                            xtype: 'label',
+                                            html: '预约持续时间:180'
+                                        },
+                                        {
+                                            xtype: 'label',
+                                            html: '用户地址:xxxxxxxxxxxxxx'
                                         }
                                     ]
+                                },
+                                {
+                                    xtype: 'button',
+                                    ui: 'decline',
+                                    width: 140,
+                                    height: 100,
+                                    text: '订单详情',
+                                    handler: function () {
+                                        panel = Ext.create('Worker.view.orderPage.OrderDetail',{title:'23232323'});
+                                        Ext.getCmp('mainView').push(panel);
+                                    }
                                 }
                             ]
                         }
                     ]
-                }]
+                }
+            ]
+
             }
         ]
     }
-})
-;
+});
